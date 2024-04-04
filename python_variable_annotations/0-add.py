1e7483db478da6de621d723bdc795b8b3f1b87f5
feeruzpanahaliyev <156419117+feeruzpanahaliyev@users.noreply.github.com>
2024-04-04 12:01:09 +0400

    Update 0-add.py

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,16 +1,7 @@
 #!/usr/bin/env python3
-
+"""a type-annotated function float typed"""
 
 
 def add(a: float, b: float) -> float:
-    """
-    Adds two floating-point numbers.
-
-    Args:
-        a (float): The first floating-point number.
-        b (float): The second floating-point number.
-
-    Returns:
-        float: The sum of the two input numbers.
-    """
+    """returns the sum of two floats"""
     return a + b
