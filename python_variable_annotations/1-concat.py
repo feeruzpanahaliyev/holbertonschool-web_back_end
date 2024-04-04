515b46fea6719d195b905694e3b78e9f743d7e8a
feeruzpanahaliyev <156419117+feeruzpanahaliyev@users.noreply.github.com>
2024-04-04 12:08:52 +0400

    Update 1-concat.py

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -2,6 +2,6 @@
 """a type-annotated function str typed"""
 
 
-def add(str1: str, str2: str) -> str:
+def concat(str1: str, str2: str) -> str:
     """returns the sum of two string"""
     return str1 + str2
