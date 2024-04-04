9e9a737a158cda6a744b501ccc9c9ce65121f4d3
feeruzpanahaliyev <156419117+feeruzpanahaliyev@users.noreply.github.com>
2024-04-04 18:55:11 +0400

    Update 5-sum_list.py

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """a type-annotated function float typed"""
-from typing import List, Union
+from typing import List
 
 
-def sum_list(input_list: List[Union[int, float]]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """returns float sum of input list of floats"""
 
     sum: float = 0
