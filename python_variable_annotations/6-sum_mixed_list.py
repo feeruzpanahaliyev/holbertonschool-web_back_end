2b0d6cd6873629337a58c32e73f1201824b53632
feeruzpanahaliyev <156419117+feeruzpanahaliyev@users.noreply.github.com>
2024-04-04 13:59:22 +0400

    Add files via upload

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""a type-annotated function float typed"""
+from typing import List
+
+
+def sum_list(mxd_list: List[int, float]) -> float:
+    """returns float sum of input list of floats and ints"""
+
+    sum = 0
+
+    for l in mxd_list:
+        sum += l
+
+    return sum
