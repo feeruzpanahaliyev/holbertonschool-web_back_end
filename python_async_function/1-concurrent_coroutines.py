04136eff6391420c08ea82cfabf42851e7944bf8
feeruzpanahaliyev <156419117+feeruzpanahaliyev@users.noreply.github.com>
2024-04-10 18:56:38 +0400

    Update 1-concurrent_coroutines.py

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -21,5 +21,5 @@ async def wait_n(n: int = 0, max_delay: int = 10) -> List[float]:
     for task in asyncio.as_completed((tasks)):
         delay = await task
         delays.append(delay)
-
+        
     return delays
