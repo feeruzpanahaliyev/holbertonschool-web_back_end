6f36d4bb373269cd166e0bf7879185c5b592a599
feeruzpanahaliyev <156419117+feeruzpanahaliyev@users.noreply.github.com>
2024-04-10 18:54:47 +0400

    Update 1-concurrent_coroutines.py

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""" continue at the same time witha sync """
+""" continue at the same time with async """
 import asyncio
 from typing import List
 
@@ -8,7 +8,9 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int = 0, max_delay: int = 10) -> List[float]:
-    """ float time random """
+    """
+    float time random
+    """
     delays: List[float] = []
     tasks: List = []
 
