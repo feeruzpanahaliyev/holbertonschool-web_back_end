673d22acceb7d45c241a3388f4ff5d7a46771dc8
feeruzpanahaliyev <156419117+feeruzpanahaliyev@users.noreply.github.com>
2024-04-10 14:08:41 +0400

    Update 8-make_multiplier.py

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-"""This import statement imports the Callable type hint from the typing module"""
+"""imports the Callable type hint from the typing module"""
 from typing import Callable
 
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
-    """Returns a function that multiplies a float by the specified multiplier."""
+    """Returns a func that multiplies a float by the specified multi"""
 
     def multiply(num: float) -> float:
         return num * multiplier
