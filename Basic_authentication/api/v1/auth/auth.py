c6f2770d3bdf648ca91d6475d940e3b5f0f43958
feeruzpanahaliyev <156419117+feeruzpanahaliyev@users.noreply.github.com>
2024-04-21 22:49:16 +0400

    Update auth.py

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -1,71 +1,38 @@
 #!/usr/bin/env python3
-""" Module of auth
-"""
+'''
+manage the API authentification
+'''
 from flask import request
-from typing import List, TypeVar
+from typing import TypeVar, List
 
 
-class Auth:
-    """ Auth Class """
-
-    def __init__(self):
-        """
-            Constructor
-
-            Args:
-                path: path to authenticate
-                excluded_paths: list of excluded path to authenticate
-        """
-
+class Auth():
+    '''
+    manage the API authentification
+    '''
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """
-            Require the auth
-
-            Args:
-                path: path to authenticate
-                excluded_paths: list of excluded path to authenticate
-
-            Return:
-                True if is authenticated otherwise false
-        """
-        if path is None or excluded_paths is None or len(excluded_paths) == 0:
+        ''' require_authentification '''
+        if path is None:
             return True
-
-        if path[-1] is not '/':
-            path += '/'
-
-        for paths in excluded_paths:
-            if paths.endswith('*'):
-                if path.startswith(paths[:-1]):
-                    return False
-            elif path == paths:
-                return False
-
+        if excluded_paths is None:
+            return True
+        if len(excluded_paths) == 0:
+            return True
+        if path is None or excluded_paths is None:
+            return True
+        path = path + '/' if path[-1] != '/' else path
+        if path in excluded_paths:
+            return False
         return True
 
     def authorization_header(self, request=None) -> str:
-        """
-            Look the headers
-
-            Args:
-                request: Look the autthorization
-
-            Return:
-                The authorization header or None
-        """
+        ''' def authorization_header '''
         if request is None:
             return None
-
-        return request.headers.get('Authorization', None)
+        if 'Authorization' not in request.headers:
+            return None
+        return request.headers['Authorization']
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """
-            Look current user
-
-            Args:
-                request: Look the reques user
-
-            Return:
-                The user
-        """
-        return request
+        ''' def current_user '''
+        return None
