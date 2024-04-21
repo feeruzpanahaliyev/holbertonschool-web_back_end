0ff2d656d9dbe2dfa72715dc5ca995727c442d87
feeruzpanahaliyev <156419117+feeruzpanahaliyev@users.noreply.github.com>
2024-04-21 22:53:34 +0400

    Update app.py

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -4,10 +4,10 @@ Route module for the API
 """
 from os import getenv
 from api.v1.views import app_views
-from api.v1.auth.auth import Auth
-from api.v1.auth.basic_auth import BasicAuth
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
+from api.v1.auth.auth import Auth
+from api.v1.auth.basic_auth import BasicAuth
 import os
 
 
@@ -15,26 +15,11 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
-if os.getenv("AUTH_TYPE") == "auth":
+
+if getenv("AUTH_TYPE") == "auth":
     auth = Auth()
-if os.getenv("AUTH_TYPE") == "basic_auth":
+elif getenv("AUTH_TYPE") == "basic_auth":
     auth = BasicAuth()
-
-
-@app.before_request
-def before_request():
-    """ Filtering of each request
-    """
-    excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
-                      '/api/v1/forbidden/']
-    if auth is None:
-        return
-    if not auth.require_auth(request.path, excluded_paths):
-        return
-    if auth.authorization_header(request) is None:
-        abort(401)
-    if auth.current_user(request) is None:
-        abort(403)
 
 
 @app.errorhandler(404)
@@ -45,17 +30,30 @@ def not_found(error) -> str:
 
 
 @app.errorhandler(401)
-def unauth(error) -> str:
-    """ def unauthorized
+def unauthorized(error) -> str:
+    """ Unauthorized handler
     """
     return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.errorhandler(403)
 def forbidden(error) -> str:
-    """ def forbidden
+    """ Forbidden handler
     """
     return jsonify({"error": "Forbidden"}), 403
+
+
+@app.before_request
+def before_request():
+    '''self descriptive'''
+    blacklist = ['/api/v1/status/', '/api/v1/unauthorized/',
+                 '/api/v1/forbidden/']
+
+    if auth and auth.require_auth(request.path, blacklist):
+        if not auth.authorization_header(request):
+            abort(401)
+        if not auth.current_user(request):
+            abort(403)
 
 
 if __name__ == "__main__":
